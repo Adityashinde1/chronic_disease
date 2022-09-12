e5a4ca60b7268105f0674769c14e3a4eec67102e
Adityashinde1 <shindeadi39@gmail.com>
2022-09-12 17:49:15 +0530

    files added

---
+++ app.py
@@ -0,0 +1,210 @@
+from flask import Flask, request
+import sys
+
+import pip
+from kidneyDisease.util.util import read_yaml_file, write_yaml_file
+from matplotlib.style import context
+from kidneyDisease.logger import logging
+from kidneyDisease.exception import RestuarantException
+import os, sys
+import json
+from kidneyDisease.config.configuration import Configuartion
+from kidneyDisease.constant import CONFIG_DIR, get_current_time_stamp
+from kidneyDisease.pipeline.pipeline import Pipeline
+from kidneyDisease.entity.restuarant_predictor import RestuarantPredictor, RestuarantData
+from flask import send_file, abort, render_template
+
+
+ROOT_DIR = os.getcwd()
+LOG_FOLDER_NAME = "logs"
+PIPELINE_FOLDER_NAME = "restuarant"
+SAVED_MODELS_DIR_NAME = "saved_models"
+MODEL_CONFIG_FILE_PATH = os.path.join(ROOT_DIR, CONFIG_DIR, "model.yaml")
+LOG_DIR = os.path.join(ROOT_DIR, LOG_FOLDER_NAME)
+PIPELINE_DIR = os.path.join(ROOT_DIR, PIPELINE_FOLDER_NAME)
+MODEL_DIR = os.path.join(ROOT_DIR, SAVED_MODELS_DIR_NAME)
+
+
+from kidneyDisease.logger import get_log_dataframe
+
+RESTUARANT_DATA_KEY = "restuarant_data"
+RESTUARANT_RATING_VALUE_KEY = "restuarant_rating_value"
+
+app = Flask(__name__)
+
+
+@app.route('/artifact', defaults={'req_path': 'restuarant'})
+@app.route('/artifact/<path:req_path>')
+def render_artifact_dir(req_path):
+    os.makedirs("resturant", exist_ok=True)
+    # Joining the base and the requested path
+    print(f"req_path: {req_path}")
+    abs_path = os.path.join(req_path)
+    print(abs_path)
+    # Return 404 if path doesn't exist
+    if not os.path.exists(abs_path):
+        return abort(404)
+
+    # Check if path is a file and serve
+    if os.path.isfile(abs_path):
+        if ".html" in abs_path:
+            with open(abs_path, "r", encoding="utf-8") as file:
+                content = ''
+                for line in file.readlines():
+                    content = f"{content}{line}"
+                return content
+        return send_file(abs_path)
+
+    # Show directory contents
+    files = {os.path.join(abs_path, file_name): file_name for file_name in os.listdir(abs_path) if
+             "artifact" in os.path.join(abs_path, file_name)}
+
+    result = {
+        "files": files,
+        "parent_folder": os.path.dirname(abs_path),
+        "parent_label": abs_path
+    }
+    return render_template('files.html', result=result)
+
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    try:
+        return render_template('index.html')
+    except Exception as e:
+        return str(e)
+
+
+@app.route('/view_experiment_hist', methods=['GET', 'POST'])
+def view_experiment_history():
+    experiment_df = Pipeline.get_experiments_status()
+    context = {
+        "experiment": experiment_df.to_html(classes='table table-striped col-12')
+    }
+    return render_template('experiment_history.html', context=context)
+
+
+@app.route('/train', methods=['GET', 'POST'])
+def train():
+    message = ""
+    pipeline = Pipeline(config=Configuartion(current_time_stamp=get_current_time_stamp()))
+    if not Pipeline.experiment.running_status:
+        message = "Training started."
+        pipeline.start()
+    else:
+        message = "Training is already in progress."
+    context = {
+        "experiment": pipeline.get_experiments_status().to_html(classes='table table-striped col-12'),
+        "message": message
+    }
+    return render_template('train.html', context=context)
+
+
+@app.route('/predict', methods=['GET', 'POST'])
+def predict():
+    context = {
+        RESTUARANT_DATA_KEY: None,
+        RESTUARANT_RATING_VALUE_KEY: None
+    }
+
+    if request.method == 'POST':
+        votes = float(request.form['votes'])
+        average_cost_for_two = float(request.form['average_cost_for_two'])
+        has_table_booking = float(request.form['has_table_booking'])
+        has_online_delivery = float(request.form['has_online_delivery'])
+        price_range = float(request.form['price_range'])
+        
+        restuarant_data = RestuarantData(votes=votes,
+                                   average_cost_for_two=average_cost_for_two,
+                                   has_online_delivery=has_online_delivery,
+                                   has_table_booking=has_table_booking,
+                                   price_range=price_range
+                                   )
+        restuarant_df = restuarant_data.get_restuarant_input_data_frame()
+        restuarant_predictor = RestuarantPredictor(model_dir=MODEL_DIR)
+        median_rating_value = restuarant_predictor.predict(X=restuarant_df)
+        context = {
+            RESTUARANT_DATA_KEY: restuarant_data.get_restuarant_data_as_dict(),
+            RESTUARANT_RATING_VALUE_KEY: median_rating_value,
+        }
+        return render_template('predict.html', context=context)
+    return render_template("predict.html", context=context)
+
+
+@app.route('/saved_models', defaults={'req_path': 'saved_models'})
+@app.route('/saved_models/<path:req_path>')
+def saved_models_dir(req_path):
+    os.makedirs("saved_models", exist_ok=True)
+    # Joining the base and the requested path
+    print(f"req_path: {req_path}")
+    abs_path = os.path.join(req_path)
+    print(abs_path)
+    # Return 404 if path doesn't exist
+    if not os.path.exists(abs_path):
+        return abort(404)
+
+    # Check if path is a file and serve
+    if os.path.isfile(abs_path):
+        return send_file(abs_path)
+
+    # Show directory contents
+    files = {os.path.join(abs_path, file): file for file in os.listdir(abs_path)}
+
+    result = {
+        "files": files,
+        "parent_folder": os.path.dirname(abs_path),
+        "parent_label": abs_path
+    }
+    return render_template('saved_models_files.html', result=result)
+
+
+@app.route("/update_model_config", methods=['GET', 'POST'])
+def update_model_config():
+    try:
+        if request.method == 'POST':
+            model_config = request.form['new_model_config']
+            model_config = model_config.replace("'", '"')
+            print(model_config)
+            model_config = json.loads(model_config)
+
+            write_yaml_file(file_path=MODEL_CONFIG_FILE_PATH, data=model_config)
+
+        model_config = read_yaml_file(file_path=MODEL_CONFIG_FILE_PATH)
+        return render_template('update_model.html', result={"model_config": model_config})
+
+    except  Exception as e:
+        logging.exception(e)
+        return str(e)
+
+
+@app.route(f'/logs', defaults={'req_path': f'{LOG_FOLDER_NAME}'})
+@app.route(f'/{LOG_FOLDER_NAME}/<path:req_path>')
+def render_log_dir(req_path):
+    os.makedirs(LOG_FOLDER_NAME, exist_ok=True)
+    # Joining the base and the requested path
+    logging.info(f"req_path: {req_path}")
+    abs_path = os.path.join(req_path)
+    print(abs_path)
+    # Return 404 if path doesn't exist
+    if not os.path.exists(abs_path):
+        return abort(404)
+
+    # Check if path is a file and serve
+    if os.path.isfile(abs_path):
+        log_df = get_log_dataframe(abs_path)
+        context = {"log": log_df.to_html(classes="table-striped", index=False)}
+        return render_template('log.html', context=context)
+
+    # Show directory contents
+    files = {os.path.join(abs_path, file): file for file in os.listdir(abs_path)}
+
+    result = {
+        "files": files,
+        "parent_folder": os.path.dirname(abs_path),
+        "parent_label": abs_path
+    }
+    return render_template('log_files.html', result=result)
+
+
+if __name__ == "__main__":
+    app.run()
---
+++ kidneyDisease/component/data_ingestion.py
@@ -0,0 +1,100 @@
+from kidneyDisease.entity.config_entity import DataIngestionConfig
+import sys,os
+from kidneyDisease.exception import RestuarantException
+from kidneyDisease.logger import logging
+from kidneyDisease.entity.artifact_entity import DataIngestionArtifact
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from six.moves import urllib
+
+class DataIngestion:
+
+    def __init__(self,data_ingestion_config:DataIngestionConfig ):
+        try:
+            logging.info(f"{'>>'*20}Data Ingestion log started.{'<<'*20} ")
+            self.data_ingestion_config = data_ingestion_config
+
+        except Exception as e:
+            raise RestuarantException(e,sys)
+    
+
+    def download_data(self,) -> str:
+        try:
+            #extraction remote url to download dataset
+            download_url = self.data_ingestion_config.dataset_download_url
+
+            #folder location to download file
+            csv_download_dir = self.data_ingestion_config.csv_download_dir
+            
+            os.makedirs(csv_download_dir,exist_ok=True)
+
+            housing_file_name = os.path.basename(download_url)
+
+            csv_file_path = os.path.join(csv_download_dir, housing_file_name)
+
+            logging.info(f"Downloading file from :[{download_url}] into :[{csv_file_path}]")
+            urllib.request.urlretrieve(download_url, csv_file_path)
+            logging.info(f"File :[{csv_file_path}] has been downloaded successfully.")
+            return csv_file_path
+
+        except Exception as e:
+            raise RestuarantException(e,sys) from e
+
+    
+    def split_data_as_train_test(self) -> DataIngestionArtifact:
+        try:
+            csv_data_dir = self.data_ingestion_config.csv_download_dir
+
+            file_name = os.listdir(csv_data_dir)[0]
+
+            zomato_file_path = os.path.join(csv_data_dir,file_name)
+
+
+            logging.info(f"Reading csv file: [{zomato_file_path}]")
+            zomato_dataframe = pd.read_csv(zomato_file_path)
+
+            logging.info(f"Splitting data into train and test")
+
+            # Train test split
+            train_set, test_set = train_test_split(zomato_dataframe, test_size=0.2, random_state=1)
+
+            train_file_path = os.path.join(self.data_ingestion_config.ingested_train_dir,
+                                            file_name)
+
+            test_file_path = os.path.join(self.data_ingestion_config.ingested_test_dir,
+                                        file_name)
+
+            if train_set is not None:
+                os.makedirs(self.data_ingestion_config.ingested_train_dir,exist_ok=True)
+                logging.info(f"Exporting training datset to file: [{train_file_path}]")
+                train_set.to_csv(train_file_path,index=False)
+
+            if test_set is not None:
+                os.makedirs(self.data_ingestion_config.ingested_test_dir, exist_ok= True)
+                logging.info(f"Exporting test dataset to file: [{test_file_path}]")
+                test_set.to_csv(test_file_path,index=False)            
+
+
+            data_ingestion_artifact = DataIngestionArtifact(train_file_path=train_file_path,
+                                test_file_path=test_file_path,
+                                is_ingested=True,
+                                message=f"Data ingestion completed successfully."
+                                )
+            logging.info(f"Data Ingestion artifact:[{data_ingestion_artifact}]")
+            return data_ingestion_artifact
+
+        except Exception as e:
+            raise RestuarantException(e,sys) from e
+
+
+    def initiate_data_ingestion(self)-> DataIngestionArtifact:
+        try:
+            csv_file_path =  self.download_data()
+            return self.split_data_as_train_test()
+        except Exception as e:
+            raise RestuarantException(e,sys) from e
+    
+
+
+    def __del__(self):
+        logging.info(f"{'>>'*20}Data Ingestion log completed.{'<<'*20} \n\n")
